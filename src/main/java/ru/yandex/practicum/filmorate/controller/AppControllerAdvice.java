6ddb03049926e17414bd209388644f40db36dfc2
shishkin <alexconermail@gmail.com>
2025-01-13 23:49:38 +0300

    github code style fix

--- src/main/java/ru/yandex/practicum/filmorate/controller/AppControllerAdvice.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/AppControllerAdvice.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 
 import java.util.HashMap;
 import java.util.List;
